d7f12439de412e4359a0a84432faa29e2101b691
WhyATent <WWyatnt@gmail.com>
2026-04-22 13:24:12 -0700

    a64 Done... but it was already good before I created it? I just changed around the format in which we can swap the numbers to the correct positions.

--- main.hpp
+++ main.hpp
@@ -8,25 +8,30 @@ void swapValues(int &, int &, int &, int &);
 void swapValues(int &n1, int &n2)
 {
     // TODO
-    swap(n1, n2);
+    int tmp;
+    tmp = n1;
+    n1 = n2;
+    n2 = tmp;
     // END TODO
 }
 
 void swapValues(int &n1, int &n2, int &n3)
 {
     // TODO
-    int tmp;
-    tmp = n1;
-    n1 = n2;
-    n2 = n3;
-    n3 = tmp;
+    swap(n1, n3);
+    swap (n2, n1);
     // END TODO
 }
 
 void swapValues(int &n1, int &n2, int &n3, int &n4)
 {
     // TODO
-    swap(n1, n4);
-    swap(n2, n3);
+    int tmp;
+    tmp = n1;
+    n1 = n4;
+    n4 = tmp;
+    tmp = n2;
+    n2 = n3;
+    n3 = tmp;
     // END TODO
 }
